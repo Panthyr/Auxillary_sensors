517a8197fcb44000c6210063437211d31fd41639
dietervansteenwegen <github@vansteenwegen.org>
2024-06-14 14:47:20 +0200

    Added `__version__` info in `__init__.py`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog auxillary sensors
 
+## Not released yet
+
+- Added `__version__` info in `__init__.py`
+
 ## v0.1.0
 
 - Initial release
--- src/auxillary_sensors/__init__.py
+++ src/auxillary_sensors/__init__.py
@@ -6,5 +6,6 @@ __author__ = 'Dieter Vansteenwegen'
 __email__ = 'dieter.vansteenwegen@vliz.be'
 __project__ = 'Panthyr'
 __project_link__ = 'https://waterhypernet.org/equipment/'
+__version__ = '0.1.0'
 
 from auxillary_sensors.auxillary import pAuxillarySensors  # noqa: F401
